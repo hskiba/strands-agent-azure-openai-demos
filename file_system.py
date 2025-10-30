22c2cd67bfe50e4b02d9705f1c6e2cc91d65ddd8
Henry Skiba <henry.skiba@frgrisk.com>
2025-10-30 10:42:51 +0800

    update demo to include AWS

--- file_system.py
+++ file_system.py
@@ -1,4 +1,5 @@
 from strands import Agent
+from strands.models.bedrock import BedrockModel
 from strands_tools import file_read, file_write, editor, shell
 
 # Define a focused system prompt for file operations
@@ -15,8 +16,18 @@ Key Capabilities:
 Always specify the full file path in your responses for clarity.
 """
 
+ANTHROPIC_SONNET = "us.anthropic.claude-sonnet-4-5-20250929-v1:0"
+ANTHROPIC_HAIKU = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
+
+model = BedrockModel(
+    model_id=ANTHROPIC_SONNET,
+    include_tool_result_status=True,
+    region_name="us-east-2",
+)
+
 # Create a file-focused agent with selected tools
 file_agent = Agent(
+    model=model,
     system_prompt=FILE_SYSTEM_PROMPT,
     tools=[file_read, file_write, editor, shell],
 )
